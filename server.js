caab7c93d31dbe2c92e1446dd3360ddf90c040b3
John Liu <stoic.llama@gmail.com>
2024-07-13 19:44:26 -0400

    small cleanup

--- server.js
+++ server.js
@@ -1,40 +1,14 @@
 require('dotenv').config()
 
-// const express = require('express')
-// const app = express()
 const app = require('./app.js')
 const mongoose = require('mongoose')
 const PORT = process.env.PORT || 9999;
 
 
-// const main = async () => {
-//   //////////////////
-//   //   Database   //
-//   //////////////////
+/////////////////
+//   Startup   //
+/////////////////
 
-//   // setup database connection
-//   mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
-//   const db = mongoose.connection 
-//   db.on('error', async (error) => console.error(error))
-//   db.once('open', async () => console.log('Connected to database'))
-
-
-//   /////////////////
-//   //   Startup   //
-//   /////////////////
-//   const port = process.env.PORT || 9999
-
-//   // start server at port 5000
-//   // app.listen(port, () => console.log(`Server started at ${port}`))
-//   await app.listen(port)
-//   await console.log(`Server started at ${port}`)
-// };
-
-// main();
-
-
-
-/* Connecting to the database and then starting the server. */
 mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => {
