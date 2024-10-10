f82a71a06a10a37fb1791fb96b1bd9d7675ef368
Stivi Cangonj <stivi.cangonj@gmail.com>
2024-10-10 00:27:42 -0400

    Added the new route for likes

--- routes/route.js
+++ routes/route.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const { healthcheck } = require('../controllers/healthcheckController.js')
 const { storesWrapper } = require('../controllers/storesController.js')
-
+const { upsDowns } = require('../controllers/likeController.js')
 
 /** HTTP Reqeust */
 router.get('/healthcheck', healthcheck)
 router.get('/product/stores', storesWrapper)
+router.patch('/likes', upsDowns)
 
 
 module.exports = router
