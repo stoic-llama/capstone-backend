c3fe4ba82fecbcd1086e6e9306ef3bac7350f104
Stivi Cangonj <stivi.cangonj@gmail.com>
2024-10-24 23:07:01 -0400

    Updated route path

--- routes/route.js
+++ routes/route.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const { healthcheck } = require('../controllers/healthcheckController.js')
 const { storesWrapper } = require('../controllers/storesController.js')
-const { upsDowns } = require('../controllers/likeController.js')
+const { updateLikes } = require('../controllers/likeController.js')
 
 /** HTTP Reqeust */
 router.get('/healthcheck', healthcheck)
 router.get('/product/stores', storesWrapper)
-router.patch('/likes', upsDowns)
+router.patch('/product/updatelike', updateLikes)
 
 
 module.exports = router
