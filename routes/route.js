381a4c8ba91bef63fb75368f1530681cfc528d9f
Stivi Cangonj <stivi.cangonj@gmail.com>
2024-10-24 23:42:49 -0400

    Added dislike route - updated like route path name

--- routes/route.js
+++ routes/route.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const { healthcheck } = require('../controllers/healthcheckController.js')
 const { storesWrapper } = require('../controllers/storesController.js')
 const { updateLikes } = require('../controllers/likeController.js')
+const { updateDislikes } = require('../controllers/dislikeController.js')
 
 /** HTTP Reqeust */
 router.get('/healthcheck', healthcheck)
 router.get('/product/stores', storesWrapper)
-router.patch('/product/updatelike', updateLikes)
+router.patch('/product/like', updateLikes)
+router.patch('/product/dislike', updateDislikes)
 
 
 module.exports = router
