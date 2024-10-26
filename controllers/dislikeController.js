f013ee11e397ace287978c0afcfca823ce8d8dd7
John Liu <stoic.llama@gmail.com>
2024-10-26 13:41:32 -0400

    Updated variable names - added success return message

--- controllers/dislikeController.js
+++ controllers/dislikeController.js
@@ -5,16 +5,16 @@ const store_item = require('../models/store_item')
 
 const updateDislikes = async (req, res) => 
 {
-    const { storeId, productId, email, like, dislike } = req.body; 
-    console.log('Received parameters:', { storeId, productId, email, like, dislike });
+    const { store_id, product_id, email, like, dislike } = req.body; 
+    console.log('Received parameters:', { store_id, product_id, email, like, dislike });
 
     if ( (dislike == 1) && (like == 0)   )
     {
         try {
             const store = await Store.findOneAndUpdate(
                 { 
-                    _id: new mongoose.Types.ObjectId(storeId), 
-                    "Store_items._id": new mongoose.Types.ObjectId(productId) 
+                    _id: new mongoose.Types.ObjectId(store_id), 
+                    "Store_items._id": new mongoose.Types.ObjectId(product_id) 
                 },
                 {
                     $addToSet: { "Store_items.$.Dislikes": email }, // Add email to Likes
@@ -24,7 +24,7 @@ const updateDislikes = async (req, res) =>
             );
 
             if (!store) {
-                console.log('Store not found for:', { storeId, productId });
+                console.log('Store not found for:', { storeId: store_id, productId: product_id });
                 return res.status(404).json({ message: 'Store or product not found' });
             }
 
@@ -38,19 +38,20 @@ const updateDislikes = async (req, res) =>
         try {
             const store = await Store.findOneAndUpdate(
                 { 
-                    _id: new mongoose.Types.ObjectId(storeId), 
-                    "Store_items._id": new mongoose.Types.ObjectId(productId) 
+                    _id: new mongoose.Types.ObjectId(store_id), 
+                    "Store_items._id": new mongoose.Types.ObjectId(product_id) 
                 },
                 {
                     $pull: {"Store_items.$.Likes": email },   // Remove user's email previous Like interaction 
                     $addToSet: { "Store_items.$.Dislikes": email }, // Add email to Dislikes
                     $inc: { "Store_items.$.Total_dislikes": 1, "Store_items.$.Total_likes": -1 } // Increment Total_dislikes and decrement Total_likes
                 },
-                { new: true } // Return the updated document instead of the original before update
+                //{ new: true } // Return the updated document instead of the original before update
             );
+            return res.status(200).json({ update_status: 'SUCCESS' });
 
             if (!store) {
-                console.log('Store not found for:', { storeId, productId });
+                console.log('Store not found for:', { storeId: store_id, productId: product_id });
                 return res.status(404).json({ message: 'Store or product not found' });
             }
 
